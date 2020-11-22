da32496a7f91d4db207acb88c81f705165cc82ae
yasinkilinc <ykilinc@fitekno.com>
2020-11-23 00:07:16 +0300

    layout added. translate added.

--- src/web-ui/src/app/app-routing.module.ts
+++ src/web-ui/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component';
 import {DashboardModule} from './pages/dashboard/dashboard.module';
 import {ProjectModule} from './pages/project/project.module';
 import {IssueModule} from './pages/issue/issue.module';
+import {AppLayoutComponent} from './_layout/app-layout/app-layout.component';
 
 const routes: Routes = [
   {
-    path:'',
+    path:'',component: AppLayoutComponent,
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'dashboard'},
       { path: 'dashboard', loadChildren: ()=> DashboardModule },
--- src/web-ui/src/app/app.module.ts
+++ src/web-ui/src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppLayoutComponent} from "./_layout/app-layout/app-layout.component";
+import {FooterComponent, SidebarComponent} from "./_layout";
+import {AppComponent} from "./app.component";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HeaderComponent} from "./_layout/header/header.component";
+
+export const createTranslateLoader = (http: HttpClient) => {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
