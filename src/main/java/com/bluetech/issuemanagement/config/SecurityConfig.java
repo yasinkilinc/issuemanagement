61d22a51a2e43c8b4785fcb52355966a8c3c8900
yasinkilinc <yasinkilinc05@gmail.com>
2020-12-11 19:02:49 +0300

    SecurityConfig added.
    Signed-off-by: yasinkilinc <yasinkilinc05@gmail.com>

---
+++ src/main/java/com/bluetech/issuemanagement/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.bluetech.issuemanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/*
+ * Created by yasinkilinc on 6.12.2020
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+
+}
